94e61cee017449dc95bb86e66c62661613b32fbb
muyexi <muyexi@gmail.com>
2018-06-21 14:27:45 +0800

    refactor(UIViewController+Options): `defaultOptions` instead of associated value

--- Source/UIViewController+Options.swift
+++ Source/UIViewController+Options.swift
@@ -1,36 +1,33 @@
 import UIKit
 
-private var CustomOptions = "CustomOptions"
-private var DefaultsOptions = "DefaultsOptions"
+var CustomOptions = "CustomOptions"
 
 extension UIViewController {
+    
+    var defaultOptions: [SemiModalOption: Any] {
+        return [
+            .traverseParentHierarchy : true,
+            .pushParentBack          : false,
+            .animationDuration       : 0.5,
+            .parentAlpha             : 0.5,
+            .parentScale             : 0.8,
+            .shadowOpacity           : 0.5,
+            .transitionStyle         : SemiModalTransitionStyle.slideUp,
+            .disableCancel           : true
+        ]
+    }
   
     func registerOptions(_ options: [SemiModalOption: Any]?) {
-        registerOptions(options, defaults: [
-                .traverseParentHierarchy : true,
-                .pushParentBack          : false,
-                .animationDuration       : 0.5,
-                .parentAlpha             : 0.5,
-                .parentScale             : 0.8,
-                .shadowOpacity           : 0.5,
-                .transitionStyle         : SemiModalTransitionStyle.slideUp,
-                .disableCancel           : true
-            ])
-    }
-    
-    func registerOptions(_ options: [SemiModalOption: Any]?, defaults: [SemiModalOption: Any]) {
         objc_setAssociatedObject(self, &CustomOptions, options, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
-        objc_setAssociatedObject(self, &DefaultsOptions, defaults, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
     }
     
     func optionForKey(_ optionKey: SemiModalOption) -> Any? {
         let options = objc_getAssociatedObject(self, &CustomOptions) as? [SemiModalOption: Any]
-        let defaults = objc_getAssociatedObject(self, &DefaultsOptions) as? [SemiModalOption: Any]
       
         if options?[optionKey] != nil {
             return options?[optionKey]
         } else {
-            return defaults?[optionKey]
+            return defaultOptions[optionKey]
         }
     }
 
